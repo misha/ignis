5f402a3bee59f52f12d4596f38ce7f4d7ec42512
Misha Soliterman <msoliter@gmail.com>
2026-08-05 18:52:33 +0900

    fix: Set the example project's app ID to jp.misha.ignis.

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.example"
+    namespace = "jp.misha.ignis"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.example"
+        applicationId = "jp.misha.ignis"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.example
+package jp.misha.ignis
 
 import io.flutter.embedding.android.FlutterActivity
 
