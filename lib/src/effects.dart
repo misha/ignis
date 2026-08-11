48fdfe9f03aace43c9f21a71b1fd50c46c963a11
Misha Soliterman <msoliter@gmail.com>
2026-08-11 10:49:10 +0900

    remove: GlowEffect. A little too naive of an implementation.

--- lib/src/effects.dart
+++ lib/src/effects.dart
@@ -1,7 +1,6 @@
 export 'effects/anchor_effect.dart';
 export 'effects/combined_effect.dart';
 export 'effects/controlled_effect.dart';
-export 'effects/glow_effect.dart';
 export 'effects/move_effect.dart';
 export 'effects/opacity_effect.dart';
 export 'effects/rotate_effect.dart';
