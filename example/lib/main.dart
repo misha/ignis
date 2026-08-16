8da870d78752370a1e439f78ad9096b855ba291d
Misha Soliterman <msoliter@gmail.com>
2026-08-16 10:15:11 +0900

    wip

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:ignis/ignis.dart';
@@ -32,15 +34,13 @@ class GamePage extends HookWidget {
 }
 
 class GameNode extends Node {
-  GameNode() {
-    final shape = add(
-      ShapeNode(
-        shape: .square(100),
-        anchor: .center,
-        paint: .new()..color = Colors.blue,
-      ),
-    );
+  final shape = ShapeNode(
+    shape: .square(100),
+    anchor: .center,
+    paint: .new()..color = Colors.blue,
+  );
 
+  GameNode() {
     Unwatch? unwatchResize;
 
     onMount(() {
@@ -52,5 +52,12 @@ class GameNode extends Node {
     onUnmount(() {
       unwatchResize?.call();
     });
+
+    add(shape);
+  }
+
+  @override
+  void tick(double dt) {
+    shape.angle += pi / 4 * dt;
   }
 }
