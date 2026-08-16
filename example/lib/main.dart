352cf8ecf05582eda5e4e7761ea774aedbc89821
Misha Soliterman <msoliter@gmail.com>
2026-08-16 10:10:45 +0900

    new: Add an app to probe live reload.

---
+++ example/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:ignis/ignis.dart';
+
+void main() {
+  runApp(const LiveReloadApp());
+}
+
+class LiveReloadApp extends StatelessWidget {
+  const LiveReloadApp({super.key});
+
+  @override
+  Widget build(context) {
+    return const MaterialApp(
+      title: 'Live Reload',
+      home: GamePage(),
+    );
+  }
+}
+
+class GamePage extends HookWidget {
+  const GamePage({super.key});
+
+  @override
+  Widget build(context) {
+    final game = useMemoized(() => GameNode());
+
+    return Scaffold(
+      body: SceneWidget(game.mount()),
+    );
+  }
+}
+
+class GameNode extends Node {
+  GameNode() {
+    final shape = add(
+      ShapeNode(
+        shape: .square(100),
+        anchor: .center,
+        paint: .new()..color = Colors.blue,
+      ),
+    );
+
+    Unwatch? unwatchResize;
+
+    onMount(() {
+      unwatchResize = scene.onResize((size) {
+        shape.position.setFrom(size / 2);
+      });
+    });
+
+    onUnmount(() {
+      unwatchResize?.call();
+    });
+  }
+}
